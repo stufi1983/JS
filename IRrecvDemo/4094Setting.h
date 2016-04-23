c5ceaf3c65dbc92e2d135fcea632e2b8491bb3ae
stufi1983 <stufi1983@gmail.com>
2016-04-23 15:11:40 +0700

    Issue: AddTimer & Sparate Config File
    + Display Clock and tick
    + Schedule file
    = Port datetime differ from schedule
    + Buzzer and led

--- IRrecvDemo/4094Setting.h
+++ IRrecvDemo/4094Setting.h
@@ -5,21 +5,53 @@
 #define strobePin 8
 #define dataPin  9
 #define clockPin  10
-
 byte segChar[] = {0x04, 0x2f, 0x18, 0x09, 0x23, 0x41, 0x40, 0x0f, 0x00, 0x01};
 #define STARTDIGIT digitalWrite(strobePin, LOW);
 #define ENDDIGIT digitalWrite(strobePin, HIGH);
+
+#define strobePinThn 11
+#define dataPinThn  12
+#define clockPinThn  13
+byte segCharThn[] = {0x7b,0x12,0x67,0x37,0x1e,0x3d,0x7d,0x13,0x7f,0x3f};
+#define STARTDIGITTHN digitalWrite(strobePinThn, LOW);
+#define ENDDIGITTHN digitalWrite(strobePinThn, HIGH);
 
 void enable4094(){
   pinMode(strobePin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  pinMode(strobePinThn, OUTPUT);
+  pinMode(clockPinThn, OUTPUT);
+  pinMode(dataPinThn, OUTPUT);
 }
+
+
+void displayDigitThn(byte digit) {
+  shiftOut(dataPinThn, clockPinThn, MSBFIRST, segCharThn[digit]);
+}
+void displayJamMenitThn(byte jam, byte menit, byte detik) {
+  displayDigitThn(detik % 10);
+  displayDigitThn(detik / 10);
+  displayDigitThn(menit % 10);
+  displayDigitThn(menit / 10);
+  displayDigitThn(jam % 10);
+  displayDigitThn(jam / 10);
+}
+void displayTglBulanTahun(byte tgl, byte bln, byte tahun) {
+  displayDigitThn(tahun % 10);
+  displayDigitThn(tahun / 10);
+  displayDigitThn(0);
+  displayDigitThn(2);
+  displayDigitThn(bln % 10);
+  displayDigitThn(bln / 10);
+  displayDigitThn(tgl % 10);
+  displayDigitThn(tgl / 10);
+}
+
 
 void displayDigit(byte digit) {
   shiftOut(dataPin, clockPin, MSBFIRST, segChar[digit]);
 }
-
 void displayJamMenit(byte jam, byte menit) {
   int angka = jam * 100 + menit;
   displayDigit(menit % 10);
@@ -27,9 +59,7 @@ void displayJamMenit(byte jam, byte menit) {
   displayDigit(jam % 10);
   displayDigit(jam / 10);
 }
-
 void displayMenitDetik(byte menit, byte detik) {
   displayJamMenit(menit, detik);
 }
-
 
--- IRrecvDemo/IRrecvDemo.ino
+++ IRrecvDemo/IRrecvDemo.ino
@@ -9,12 +9,31 @@ long rsl = 0;
 #define DS3231_I2C_ADDRESS 0x68
 #include "RTCSetting.h"
 byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
-
+bool dateUpdate = false; byte lastDay = 0; byte lastMonth = 0;
 
 #include <TimerOne.h>
 volatile bool timeUpdate = false;
-enum mode {RUN, EDIT};
+enum mode {RUN, EDIT, ALARM, STDBY};
 byte mode = EDIT;
+
+#include <avr/pgmspace.h>
+#include "jadwal_2.h"
+
+#define IQOMAT 10//300 //detik                                                            
+#define BUZZERON 3 //detik
+#define BUZZERSHOLAT 5 //kali
+#define pinBuzzer 7
+boolean buzzerStatus = false;
+
+#define led 6
+boolean ledStat = false;  //also as flag every 1 second
+
+static int tempReg = 0; //temporary iqomat timer in second
+
+static  uint8_t dataJam[7];
+static  uint8_t dataMenit[7];
+
+static unsigned char lastMinute = 0;
 
 void setup()
 {
@@ -28,9 +47,16 @@ void setup()
   Timer1.initialize(1000000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
   Timer1.attachInterrupt( timerIsr ); // attach the service routine here
 
+  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  dateUpdate = true;
+
+  //mode =  ALARM; tempReg = IQOMAT;
+  mode =  STDBY; tempReg = 0;
+
+  pinMode(pinBuzzer, OUTPUT);
 }
 
-
+byte i = 0;
 void loop() {
   if (REMOTEPRESS) {
     if (results.value != 0xFFFFFFFF)
@@ -44,17 +70,112 @@ void loop() {
 
   if (timeUpdate) {
     readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
-    STARTDIGIT
-    if (mode == EDIT)
-      displayMenitDetik(minute, second);
-    if (mode == RUN)
-      displayJamMenit(hour, minute);
-    ENDDIGIT
-    timeUpdate = false;
-  }
-  Serial.println(second);
 
+    STARTDIGITTHN
+    displayTglBulanTahun(dayOfMonth, month, year);
+    displayJamMenitThn(hour, minute, second);
+    ENDDIGITTHN
+
+    updateJadwal(month, dayOfMonth, hour, minute);
+
+    timeUpdate = false;
+    if (tempReg > (0 - BUZZERSHOLAT)) {
+      tempReg--;
+    }
+
+    ledStat = !ledStat;
+
+    if ((lastDay != dayOfMonth) && (lastMonth != month)) {
+      dateUpdate = true;
+    }
+
+  }
+
+  if (dateUpdate) {
+    dataJadwal(month, dayOfMonth);
+
+    STARTDIGIT
+    for (int i = 1; i <= 6; i++) { //array start from 1
+      displayJamMenit(dataJam[i], dataMenit[i]);
+    }
+    ENDDIGIT
+
+    lastDay = dayOfMonth; lastMonth = month;
+    dateUpdate = false;
+  }
+
+
+  if (mode == ALARM) {
+    //Buzzer masuk waktu sholat                                      //-------------------------------------------------
+
+    if ((tempReg >= (IQOMAT - BUZZERON))) {
+      if (!buzzerStatus) {
+        buzzerStatus = HIGH;
+        Serial.println("Buzzer On ");
+      }
+    } else {
+      if (buzzerStatus) {
+        buzzerStatus = LOW;
+        Serial.println("Buzzer Off ");
+      }
+    }
+    digitalWrite(pinBuzzer, buzzerStatus);
+
+    int x = tempReg / 60;
+    if (x > 0) {  //more than 1 minute
+      digitalWrite(led, ledStat);
+      if (ledStat) {
+        Serial.print("Iqomat:");
+        Serial.print(x, DEC);
+        Serial.println(" menit");
+        delay(200);
+        ledStat = false;
+      }
+    } else if (tempReg >= 0) { //less than 1 minute
+      if (ledStat) {
+        Serial.print("Iqomat:");
+        Serial.print(tempReg, DEC);
+        Serial.println(" detik");
+        delay(200);
+        ledStat = false;
+
+        //ToDo: Display counter
+        STARTDIGIT
+        displayMenitDetik(x, tempReg % 60);
+        ENDDIGIT
+
+        if (tempReg == 0) {
+          Serial.println("Sholat time!");
+          //ToDo: Reset display counter
+          STARTDIGIT
+          for (int i = 1; i <= 6; i++) { //array start from 1
+            displayJamMenit(dataJam[i], dataMenit[i]);
+          }
+          ENDDIGIT
+
+        }
+      }
+    }
+
+
+    if (tempReg <= 0) { //if iqomat time up
+      // Prayer start ..............
+      if (ledStat) {
+        digitalWrite(pinBuzzer, HIGH);
+        Serial.print("Buzzer On ");
+        delay(500);
+        digitalWrite(pinBuzzer, LOW);
+        Serial.println("Buzzer Off ");
+        ledStat = false;
+      }
+    }
+    if (tempReg <= (0 - BUZZERSHOLAT)) {
+      mode = STDBY;
+      Serial.println("Standby!");
+    }
+  }
 }
+
 
 ///Timer
 void timerIsr()
@@ -103,3 +224,108 @@ void readDS3231time(byte *second,
 }
 
 
+void updateJadwal(uint8_t bulan, uint8_t tgl, uint8_t jam, uint8_t menit) {
+  if (checkMinuteChange(menit)) {
+    if (checkDayChange(tgl)) {
+      dataJadwal(bulan, tgl);
+      Serial.println("Update Jadwal");
+    }
+    compareRTC(jam, menit);
+  }
+
+}
+
+void dataJadwal(uint8_t bulan, uint8_t tgl) {
+  Serial.print("Update jadwal: ");
+  for (unsigned char i = 1; i <= 6; i++) {
+    int x = ( (i - 1) * 32)  + tgl;
+
+    switch (bulan) {
+      case 1: dataMenit[i] = pgm_read_byte_near(jadwalMnt1 + x); dataJam[i] = pgm_read_byte_near(jadwalJam1 + x); break;
+      case 2: dataMenit[i] = pgm_read_byte_near(jadwalMnt2 + x); dataJam[i] = pgm_read_byte_near(jadwalJam2 + x); break;
+      case 3: dataMenit[i] = pgm_read_byte_near(jadwalMnt3 + x); dataJam[i] = pgm_read_byte_near(jadwalJam3 + x); break;
+      case 4: dataMenit[i] = pgm_read_byte_near(jadwalMnt4 + x); dataJam[i] = pgm_read_byte_near(jadwalJam4 + x); break;
+      case 5: dataMenit[i] = pgm_read_byte_near(jadwalMnt5 + x); dataJam[i] = pgm_read_byte_near(jadwalJam5 + x); break;
+      case 6: dataMenit[i] = pgm_read_byte_near(jadwalMnt6 + x); dataJam[i] = pgm_read_byte_near(jadwalJam6 + x); break;
+      case 7: dataMenit[i] = pgm_read_byte_near(jadwalMnt7 + x); dataJam[i] = pgm_read_byte_near(jadwalJam7 + x); break;
+      case 8: dataMenit[i] = pgm_read_byte_near(jadwalMnt8 + x); dataJam[i] = pgm_read_byte_near(jadwalJam8 + x); break;
+      case 9: dataMenit[i] = pgm_read_byte_near(jadwalMnt9 + x); dataJam[i] = pgm_read_byte_near(jadwalJam9 + x); break;
+      case 10: dataMenit[i] = pgm_read_byte_near(jadwalMnt10 + x); dataJam[i] = pgm_read_byte_near(jadwalJam10 + x); break;
+      case 11: dataMenit[i] = pgm_read_byte_near(jadwalMnt11 + x); dataJam[i] = pgm_read_byte_near(jadwalJam11 + x); break;
+      case 12: dataMenit[i] = pgm_read_byte_near(jadwalMnt12 + x); dataJam[i] = pgm_read_byte_near(jadwalJam12 + x); break;
+      default: break;
+    }
+
+    Serial.print(dataJam[i], DEC);
+    Serial.print(":");
+    Serial.print(dataMenit[i], DEC);
+    Serial.print(" ");
+  }
+  /*
+  #if DEBUG
+  for (unsigned char i = 0; i <= 3; i++) {
+    Serial.print(pgm_read_byte_near(jadwalJI[0][i]),DEC);
+    Serial.print(":");
+    Serial.print(pgm_read_byte_near(jadwalJI[1][i]),DEC);
+    Serial.print(" ");
+  }
+  Serial.println("");
+  #endif*/
+}
+
+boolean checkMinuteChange(uint8_t menit) {
+  if (menit != lastMinute) {
+    lastMinute = menit;
+    return true;
+  } else return false;
+}
+boolean checkDayChange(uint8_t tgl) {
+  if (tgl != lastDay) {
+    lastDay = tgl;
+    return true;
+  } else return false;
+}
+
+void compareRTC(uint8_t jam, uint8_t menit) {
+  if (jam == dataJam[1] && menit == dataMenit[1]) {
+    mode = ALARM;
+    //kata = FS(kataK);
+    tempReg = IQOMAT;
+    Serial.print("Imsyak"); debugJam();
+  } else if (jam == dataJam[2] && menit == dataMenit[2]) {
+    mode = ALARM;
+    //kata = FS(kataS);
+    tempReg = IQOMAT;
+    Serial.print("Subuh"); debugJam();
+  } else if (jam == dataJam[3] && menit == dataMenit[3]) {
+    mode = ALARM;
+    //kata = FS(kataD);
+    tempReg = IQOMAT;
+    Serial.print("Dhuhur"); debugJam();
+  } else if (jam == dataJam[4] && menit == dataMenit[4]) {
+    mode = ALARM;
+    //kata = FS(kataA);
+    tempReg = IQOMAT;
+    Serial.print("Ashar"); debugJam();
+  } else if (jam == dataJam[5] && menit == dataMenit[5]) {
+    mode = ALARM;
+    //kata = FS(kataM);
+    tempReg = IQOMAT;
+    Serial.print("Magrib"); debugJam();
+  } else if (jam == dataJam[6] && menit == dataMenit[6]) {
+    mode = ALARM;
+    //kata = FS(kataI);
+    tempReg = IQOMAT;
+    Serial.print("Isya"); debugJam();
+  } else return;
+
+}
+
+void debugJam() {
+  Serial.print(year, DEC); Serial.print("/");
+  Serial.print(month, DEC); Serial.print("/");
+  Serial.print(dayOfMonth, DEC); Serial.print(" ");
+  Serial.print(hour, DEC); Serial.print(":");
+  Serial.print(minute, DEC); Serial.print(":");
+  Serial.print(second, DEC); Serial.println(" ");
+}
